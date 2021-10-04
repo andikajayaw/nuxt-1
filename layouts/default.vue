4b9355cbf55cea59634c49abc2ba626482777d5d
silvesterwali <silvesterlhwali123@gmail.com>
2021-10-04 22:02:16 +0800

    m

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-
     <div class="container">
-            <ul class="nav">
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Active</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled">Disabled</a>
-      </li>
-    </ul>
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+      
+          <nuxt-link to="/" class="nav-link" >Home</nuxt-link>
+        </li>
+        <li class="nav-item">
+           <nuxt-link to="/about" class="nav-link" >about</nuxt-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
       <nuxt />
     </div>
   </div>
