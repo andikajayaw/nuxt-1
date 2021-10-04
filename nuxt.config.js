a65f7d9a79e49adb0ba26d74f2f114322931d288
silvesterwali <silvesterlhwali123@gmail.com>
2021-10-04 21:43:22 +0800

    adding boostrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {type: 'text/css', href:"https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css", rel="stylesheet" ,crossorigin:"anonymous", integrity:"sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"}
     ]
   },
 
