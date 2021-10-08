25117c4e25c5a6e963b42d9597dfd73b947bc547
silvesterwali <silvesterlhwali123@gmail.com>
2021-10-08 21:23:34 +0800

    adding

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-  router: {
-    middleware: ['auth'],
-  },
+  // router: {
+  //   middleware: ['auth'],
+  // },
 }
