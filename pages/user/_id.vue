afc9b8d20e1f83643fa71f9b6120fc6b213b743a
silvesterwali <silvesterlhwali123@gmail.com>
2021-10-08 20:35:19 +0800

    add meta

--- pages/user/_id.vue
+++ pages/user/_id.vue
@@ -14,7 +14,22 @@ export default {
       `https://jsonplaceholder.typicode.com/users/${params.id}`
     )
     return { user: data }
+
   },
+  head: {
+      // @src https://nuxtjs.org/docs/features/meta-tags-seo/
+    title: 'my website title',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'my website description'
+      }
+    ],
+
+  }
 }
 </script>
 
