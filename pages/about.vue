3c25a9f8149db3eb189c7225788ebf8c11d4b7ef
silvesterwali <silvesterlhwali123@gmail.com>
2021-10-08 20:26:24 +0800

    add seo

--- pages/about.vue
+++ pages/about.vue
@@ -21,9 +21,24 @@ export default {
       users:[]
     }
   },
+
   async fetch(){
    const data=await this.$axios.$get('https://jsonplaceholder.typicode.com/users')
    this.users=data
+  },
+    head: {
+      // @src https://nuxtjs.org/docs/features/meta-tags-seo/
+    title: 'my website title',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'my website description'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   }
 }
 </script>
